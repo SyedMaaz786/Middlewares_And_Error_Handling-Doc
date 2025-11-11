dbe059c5f865cdf45f3aa8f3f204d6ab55fa619b
Syed Maaz <syedmaaz78622@gmail.com>
2025-11-11 21:56:59 +0530

    Added ExpressError.js file and included some errorhandling concepts

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const ExpressError = require('./ExpressError');
 
 app.use( (req,res,next) => {    
     console.log('Hi, I am middleware');   //This is the middleware chaining here so when ever you execute this and run the server only this line of code will be executed and middleware will not let the other two roots which are below this to execute.
@@ -11,9 +12,14 @@ app.use( (req,res,next) => {
 app.use('/api', (req,res, next) => {        //we are creating a middleware here if the query is passed correctly in the search URL then the /api(route) msg will be displayed.
     let {query} = req.query;
     if (query === 'letmein') {
-       return next();
-    }
-    res.send('ACCESS DENIED!');
+      next();
+    }                                                    //401 is the status and ACCESS DENIED BUDDY is the message we passed in the constructor.
+    throw new ExpressError(401,'ACCESS DENIED BUDDY!');  //We also have a js builtin method to throw errors called (throw new error) if we want to throw error we can use this.
+});
+
+//Activity to throw a error when
+app.get('/admin', (req,res) => {
+    throw new ExpressError(403, 'Activity throw error');
 });
 
 app.get('/api', (req,res) => {
@@ -30,8 +36,14 @@ app.get('/random', (req,res) => {       //Created a route
 });
 
 app.use( (req,res) => {                 //We can also use middlwares to handle our error.
-    res.send('Heyy buddy check your URL there is some error in it :)') //We also have a js builtin method to throw errors called (throw new error) if we want to throw error we can use this.
-});                                                                    //I have not used this (throw new error) in this file but is it very easy simply use it where you want to throw error.
+    res.send('Heyy buddy check your URL there is some error in it :)') 
+});                                                                    
+
+
+app.use( (err,req,res,next) => {         //err is used to handle error handling or to generate a error explicitly.
+    console.log('----------ERROR----------');
+    res.send(err);
+});
 
 app.listen(8080, () => { 
     console.log('Server is listening to port 8080');
